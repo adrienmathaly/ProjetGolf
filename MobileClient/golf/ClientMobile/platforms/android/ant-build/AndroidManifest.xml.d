cdf076e3181c9e042caf9d02b1bed413fe91ff57
cedricdelhaes <delhaes.c@gmail.com>
2014-10-01 10:00:39 +0200

    Add plugin and device information
    Add plugin org.apache.cordova.device
    Display device information on index.html

---
+++ MobileClient/golf/ClientMobile/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\AndroidManifest.xml \
---
+++ MobileClient/golf/ClientMobile/platforms/android/ant-build/GolfChallenge-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\ant-build\GolfChallenge-debug-unaligned.apk : \
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\ant-build\GolfChallenge.ap_ \
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\ant-build\classes.dex \
+C:\Users\Cedric\Desktop\GolfProjectRepository\ProjetGolf\MobileClient\golf\ClientMobile\platforms\android\CordovaLib\ant-build\classes.jar \
