fe1b63e4ba9c0740fc9c96b3403df762544ea72e
Loic Trichaud <loic.trichaud@imerir.com>
2014-10-02 15:25:01 +0200

    Adjustement nearest

--- DatabaseServer/nearest.php
+++ DatabaseServer/nearest.php
@@ -5,43 +5,46 @@
 * \author Loïc TRICJAUD
 * \version 1.0
 * \date 01/10/2014
-* \brief Apres tir
+* \brief Return nearest town
 */
 
-// Récupération des données dans l'url (longitude et latitude) + affichage
-$longitude = $_GET['lg'];
-$latitude  = $_GET['lt'];
-//echo "Longitude : " . $longitude ."  - Latitude : ".$latitude;
+//      Location      |  Latitude  | Longitude
+// ============================================
+// Park next to Dijon :  47.265494 , 4.188541
+// Imerir             :  42.6746   , 2.84773
+// Canohes            :  42.641190 , 2.833076
+// Perpignan          :  42.683300 , 2.883330
+// Pic du Canigou     :  42.518881 , 2.456667     
 
-echo "<br><br>";
+
+// Data retrieving  in the url (longitude and latitude)
+$longitude  = $_GET['lg'];
+$latitude   = $_GET['lt'];
+$population = 100000;
+
+// Connection database informations
 $host = "localhost";
 $user = "root";
-$bdd  = "Ville";
+$bdd  = "DB_GLF";
 $pwd  = "";
 
-
+// Database connection
 mysql_connect($host, $user, $pwd) or die("Erreur de connexion au serveur");
 mysql_select_db($bdd) or die("Erreur de connexion a la base de donnees");
-//mysql_query("SET NAMES UTF8");
+mysql_query("SET NAMES UTF8");
 
 
-// recherche googlemap : 47.265494, 4.188541 (dijon ou autun suivant population)
-// imerir : 42.6746 , 2.84773
-//$longitude = 4.188541;
-//$latitude = 47.265494;
-
+// SQL Request 
 $query = "
     SELECT ville_nom_reel, ville_longitude_deg, ville_latitude_deg, ville_url_wiki 
     FROM 
         ( SELECT ville_nom_reel, ville_longitude_deg, ville_latitude_deg, ville_url_wiki, 
             (sqrt(pow(abs(".$longitude." - ville_longitude_deg),2) + pow(abs(".$latitude." - ville_latitude_deg), 2))) 
           FROM `t_ville` 
-          WHERE ville_population > 50000 order by 5
+          WHERE ville_population > ".$population." order by 5
         ) as tt 
     LIMIT 1 
 ";
-
-
 
 $result = mysql_query($query);
 
@@ -58,17 +61,12 @@ while($donnees = mysql_fetch_assoc($result)) {
     array_push($array_liste_ville, $ville);
 }
 
+// Json Encode
 $array_liste_ville_json = json_encode($array_liste_ville);
-//print_rr($array_liste_ville);
+
 echo ($array_liste_ville_json);
 
 mysql_close();
 
-
-function print_rr( $array ){
-	echo "<pre>";
-	print_r($array);
-	echo "</pre>";
-}
 
 ?>
