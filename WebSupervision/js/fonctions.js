3174558cd47d3cd6565fb08ae380c4587d114814
quentinmaurin <quentin.maurin@imerir.com>
2014-10-03 10:23:59 +0200

    Delete old JS functions

--- WebSupervision/js/fonctions.js
+++ WebSupervision/js/fonctions.js
@@ -84,7 +84,6 @@ function connect_to_server()
 		$("#connect").addClass("btn-info");
 		$("#connect").html("Connect");
 		document.getElementById('ipServer').disabled = false;
-		document.getElementById('button_submit').disabled = false;
 
 		//STOP THE TIMER
 		clearInterval(timer);
@@ -100,7 +99,6 @@ function connect_to_server()
 			$("#connect").addClass("btn-danger");
 			$("#connect").html("Disconnect");
 			document.getElementById('ipServer').disabled = true;
-			document.getElementById('button_submit').disabled = true;
 
 			//START THE TIMER
 			timer = setInterval( function() {HttpGET(amount_of_users)}, 500);
@@ -260,36 +258,30 @@ function delete_all_markers()
 
 function resize_map()
 {
-	var table = my_table;
 	var min_lat, max_lat, min_lng, max_lng;
 	var center_lat, center_lng;
 	var distance_lat,distance_lng;
 
 	//IF THE TABLE IS COMPLETED BY DATA
-	if (table.rows.length > 1)
+	if (my_table.rows.length > 1)
 	{
 		//ARRAY LAT ET LNG INSERTION
-		for (var j=1;j<table.rows.length;j++)
+		for (var j=1;j<my_table.rows.length;j++)
 		{
-			lat_array.push(parseFloat(table.rows[j].cells[1].innerHTML));
-			lng_array.push(parseFloat(table.rows[j].cells[2].innerHTML));
+			lat_array.push(parseFloat(my_table.rows[j].cells[1].innerHTML));
+			lng_array.push(parseFloat(my_table.rows[j].cells[2].innerHTML));
 		}	
 
-		min_lat = min(lat_array);
-		max_lat = max(lat_array);
-		min_lng = min(lng_array);
-		max_lng = max(lng_array);
+		var min_lat = min(lat_array);
+		var max_lat = max(lat_array);
+		var min_lng = min(lng_array);
+		var max_lng = max(lng_array);
 
-		distance_lat = max(lat_array) - min(lat_array);
-		distance_lng = max(lng_array) - min(lng_array);
+		var distance_lat = max(lat_array) - min(lat_array);
+		var distance_lng = max(lng_array) - min(lng_array);
 
-		center_lat = (min_lat + max_lat)/2
-		center_lng = (min_lng + max_lng)/2
-
-		//CENTER THE MAP ON THE GOOD COORDINATES
-		console.log("Center : (" + center_lat + ":" + center_lng + ")");
-		console.log("Distance lat : " + distance_lat);
-		console.log("Distance lng : " + distance_lng);
+		var center_lat = (min_lat + max_lat)/2
+		var center_lng = (min_lng + max_lng)/2
 
 		my_map.setCenter(new google.maps.LatLng(center_lat,center_lng));
 
@@ -297,86 +289,16 @@ function resize_map()
 		var array_distance = [distance_lat,distance_lng];
 		var max_distance = parseFloat(max(array_distance)).toFixed(5);
 
-		console.log("max distance : " + max_distance);
 		my_map.setZoom(choose_zoom(max_distance));
 	}
 }
 
-function choose_zoom(d)
-{
-	if 			(d >= 180)				return 1;
-	else if 	(d >= 91 && d < 180 )	return 2;
-	else if 	(d >= 49 && d < 90 )	return 3;	
-	else if 	(d >= 24 && d < 48 )	return 4;
-	else if 	(d >= 13 && d < 23 )	return 5;
-	else if 	(d >= 6 && d < 12 )		return 6;
-	else if 	(d >= 3 && d < 5 )		return 7;
-	else if 	(d >= 2 )				return 8;
-	else if 	(d >= 1 )				return 9;
-	else if 	(d >= 0.5 )				return 10;	
-	else if 	(d >= 0.2 )				return 11;	
-	else if 	(d >= 0.1 )				return 12;	
-	else if 	(d >= 0.05 )			return 13;	
-	else if 	(d >= 0.01 )			return 16;	
-	else if 	(d >= 0.005 )			return 17;
-	else if 	(d >= 0.001 )			return 19;
-	else if 	(d >= 0.0005 )			return 20;
-	else if 	(d >= 0.0001 )			return 21;
-}
-
-function min(array)
-{
-	var minimum;
-
-	if (array.length > 0)
-	{
-		minimum = array[0];
-
-		for (var i=1;i<array.length;i++)
-		{
-			if (array[i] < minimum)
-				minimum = array[i];
-		}
-	}
-	else
-		return array[0];
-
-	return minimum;
-}
-
-function max(array)
-{
-	var maximum;
-
-	if (array.length > 0)
-	{
-		maximum = array[0];
-
-		for (var i=1;i<array.length;i++)
-		{
-			if (array[i] > maximum)
-				maximum = array[i];
-		}
-	}
-	else
-		return array[0];
-
-	return maximum;
-}
 
 function hide_or_show()
 {
  	var textarea=document.getElementById('textarea_submit');
-
   	if(textarea.style.display == "none")
-  	{
-    	textarea.style.display = "block";
-    	//textarea.readOnly=true;
-    	//textarea.className ='show';
-  	}
+  	   	textarea.style.display = "block";
   	else
-  	{
     	textarea.style.display = "none";
-    	//textarea.className ='hide';
- 	}
 }
