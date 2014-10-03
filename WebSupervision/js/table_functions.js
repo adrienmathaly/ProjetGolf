655a18e5f90d6ecf6df56dd792ae8c580e4cc57d
quentinmaurin <quentin.maurin@imerir.com>
2014-10-03 10:25:52 +0200

    Delete old JS functions 2

--- WebSupervision/js/table_functions.js
+++ WebSupervision/js/table_functions.js
@@ -3,3 +3,17 @@ function add_cell()
 	
 	
 }
+
+
+function clean_table(table)
+{
+	var length = table.rows.length;
+
+	if (length > 1)
+	{
+		for (var j=1;j<length;j++)
+		{
+			table.deleteRow(1);
+		}
+	}
+}
