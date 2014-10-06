e931d9ef25bde73f197ff5cc28e45c9354cca07d
aortola <adrien.ortola@imerir.com>
2014-10-06 16:29:08 +0200

    Resolve bug linked list user

--- ApplicationServer/ServeurGolfTourism/ServerGolfTourism/src/http/handlers/supervision/UsersTravelledDistances.java
+++ ApplicationServer/ServeurGolfTourism/ServerGolfTourism/src/http/handlers/supervision/UsersTravelledDistances.java
@@ -19,6 +19,10 @@ public class UsersTravelledDistances extends CustomizedHandler  {
 
 	@Override
 	protected void doYourStuff(HttpExchange t) throws Exception {
+	//	  LinkedList<Float> list = SetOfUsers.listOfDistances();
+	//	  while(!list.isEmpty()){
+	//		  System.out.println(list.pop());
+	//	  }
 	      String response = JSONBuilder.buildJSONBasicMsg("travelledDistances",prepareListOfDistances(SetOfUsers.listOfDistances()));
 	      t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
 	      t.sendResponseHeaders(200, response.length());
@@ -30,7 +34,7 @@ public class UsersTravelledDistances extends CustomizedHandler  {
 	
 	private JSONArray prepareListOfDistances(LinkedList<Float> list){
 		JSONArray listOfDistances=new JSONArray();
-		for(int i=0; i<list.size();i++){
+		 while(!list.isEmpty()){
 			JSONObject jo = new JSONObject();
 			jo.put("dist",list.pop());
 			listOfDistances.add(jo);
